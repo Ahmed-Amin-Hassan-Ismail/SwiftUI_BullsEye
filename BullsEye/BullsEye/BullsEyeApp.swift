a880a7d718e837a22704b489cbdd3e8d0a22771e
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-09-03 22:35:42 +0300

    initial commit , create xCode with adjusting minimum deployment target

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Ahmed Amin on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
